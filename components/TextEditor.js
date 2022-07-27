0b9288aa9c7c70f480746e064fde4c3872e04c4c
Amit GUpta <amit25gupta01@gmail.com>
2022-07-27 13:34:32 +0530

    changes done

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -8,55 +8,55 @@ import { convertFromRaw, convertToRaw } from "draft-js";
 import { useSession } from "next-auth/client";
 import { useDocumentOnce } from "react-firebase-hooks/firestore";
 
-const Editor= dynamic(
+const Editor = dynamic(
     () => import("react-draft-wysiwyg").then((module) => module.Editor),
-{
-    ssr:false,
-}
+    {
+        ssr: false,
+    }
 )
 
 function TextEditor() {
-const [session]= useSession()    
-const [editorState, setEditorState]= useState(EditorState.createEmpty())
-const router = useRouter()
-const{id}= router.query
+    const [session] = useSession()
+    const [editorState, setEditorState] = useState(EditorState.createEmpty())
+    const router = useRouter()
+    const { id } = router.query
 
-const[snapshot]= useDocumentOnce(
-    db.collection("userDocs").doc(session.user.email).collection
-("docs").doc(id)
-)
+    const [snapshot] = useDocumentOnce(
+        db.collection("userDocs").doc(session.user.email).collection
+            ("docs").doc(id)
+    )
 
-useEffect(() => {
-    if(snapshot?.data()?.editorState) {
-        setEditorState(
-            EditorState.createWithContent(
-                convertFromRaw(snapshot?.data()?.editorState)
+    useEffect(() => {
+        if (snapshot?.data()?.editorState) {
+            setEditorState(
+                EditorState.createWithContent(
+                    convertFromRaw(snapshot?.data()?.editorState)
+                )
             )
-        )
+        }
+    }, [snapshot])
+
+    const onEditorStateChange = (editorState) => {
+        setEditorState(editorState)
+
+        db.collection("userDocs").doc(session.user.email).collection("docs").doc(id).set({
+            editorState: convertToRaw(editorState.getCurrentContent())
+        }, {
+            merge: true
+        })
     }
-}, [snapshot])
-
-const onEditorStateChange = (editorState) =>{
-    setEditorState(editorState)
-
-    db.collection("userDocs").doc(session.user.email).collection("docs").doc(id).set({
-      editorState: convertToRaw(editorState.getCurrentContent())  
-    },{
-        merge:true
-    })
-}
 
     return (
         <div className="bg-[#F8F9FA] min-h-screen pb-16">
-        <Editor
-        editorState={editorState}
-        onEditorStateChange={ onEditorStateChange }
+            <Editor
+                editorState={editorState}
+                onEditorStateChange={onEditorStateChange}
 
-        toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
+                toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
 
-        editorClassName="mt-6 bg-white shadow-lg max-w-5xl mx-auto mb-12 border p-10"
-        />
-            
+                editorClassName="mt-6 bg-white shadow-lg max-w-5xl mx-auto mb-12 border p-10"
+            />
+
         </div>
     )
 }
